2114fd701123593e650324a3db9cefd56deabe4d
meg <103964571+kyokosribbon@users.noreply.github.com>
2024-03-29 14:34:17 -0400

    Add files via upload

---
+++ GDES258-AS2-MeganKingston/js/script.js
@@ -0,0 +1,51 @@
+console.log("hello word!");
+
+var client = contentful.createClient({
+    space: '7iuwxqetvahg',
+    accessToken: 'C-VzpYMq056abAjyMVazLk6ylww0Km0j_ThkTfaM-wE',
+  });
+console.log(client);
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.getElementById("menu").classList.toggle("active");
+});
+
+var allProducts = document.getElementById('all-products');
+
+client.getEntries({content_type: 'gdes258As2MeganKingston',}).then(function (entries) {
+  entries.items.forEach(function (entry) {
+      var productDiv = document.createElement('div');
+
+      // header
+      var name = document.createElement('h1');
+      name.innerHTML = entry.fields.header;
+      productDiv.append(name);
+
+      // subheader
+      var name = document.createElement('h2');
+      name.innerHTML = entry.fields.subheader;
+      productDiv.append(name);
+
+      //main-image
+      var mainImage = document.createElement('img');
+      mainImage.classList.add('main-image');
+      if (entry.fields.img){
+        mainImage.src = entry.fields.img.fields.file.url;
+        productDiv.append(mainImage);
+        console.log(entry.fields.img.fields.file.url);
+      }
+
+      // description
+      var name = document.createElement('p');
+      name.innerHTML = entry.fields.paragraph;
+      productDiv.append(name);
+
+      // link
+      var name = document.createElement('a');
+      name.innerHTML = entry.fields.link;
+      productDiv.append(name);
+
+      allProducts.append(productDiv);
+
+  });
+});
